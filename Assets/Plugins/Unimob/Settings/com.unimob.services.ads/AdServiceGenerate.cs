8db36b1e9c5d13ef9b59adf684056fc603e68668
KhanhTQ-hub <khanhquang0912qh@gmail.com>
2025-04-02 10:36:17 +0700

    Plugin

---
+++ Assets/Plugins/Unimob/Settings/com.unimob.services.ads/AdServiceGenerate.cs
@@ -0,0 +1,19 @@
+﻿using com.unimob.ads.core;
+
+namespace com.unimob.services.ads
+{
+	public static class AdLocation
+	{
+		public static AdPlacement I_ElonMusk = new AdPlacement("I_ElonMusk");
+		public static AdPlacement I_ElonMuskAds = new AdPlacement("I_ElonMuskAds");
+		public static AdPlacement R_UnlockChest = new AdPlacement("R_UnlockChest");
+		public static AdPlacement R_UnlockSlot = new AdPlacement("R_UnlockSlot");
+		public static AdPlacement R_VipReward = new AdPlacement("R_VipReward");
+		public static AdPlacement R_Booster = new AdPlacement("R_Booster");
+		public static AdPlacement R_OfflineReward = new AdPlacement("R_OfflineReward");
+		public static AdPlacement R_SkillReward = new AdPlacement("R_SkillReward");
+		public static AdPlacement R_InviteHelper = new AdPlacement("R_InviteHelper");
+		public static AdPlacement R_ExtendInvitation = new AdPlacement("R_ExtendInvitation");
+		public static AdPlacement R_WaterSupplyStation = new AdPlacement("R_WaterSupplyStation");
+	}
+}
